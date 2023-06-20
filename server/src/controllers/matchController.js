30936de006f42c0db8b16cd8fe93760b2d1257c6
JoaoVittorVenturini <joaovittorventurini@outlook.com>
2023-06-20 09:40:59 -0300

    fix

--- server/src/controllers/matchController.js
+++ server/src/controllers/matchController.js
@@ -1,71 +1,71 @@
 import { openDb } from "../config/connection.js";
-import match from "../models/matchModel.js";
+import partida from "../models/matchModel.js";
 
-class matchController{
-      createMatch(){
-        openDb().then(db => {
-            db.exec('CREATE TABLE IF NOT EXISTS Match (id INTEGER PRIMARY KEY, rounds INTEGER, vencedor INTEGER, time_um INTEGER, time_dois,FOREIGN KEY(vencedor, time_um, time_dois) REFERENCES Time(id, id, id))');
-        });
-    }
-    
-    insertMatch(req, res){
-        match.rounds = req.body.rounds;
-        match.vencedor = req.body.vencedor;
-        match.time_um = req.body.time_um;
-        match.time_dois = req.body.time_dois;
-        openDb().then(db => {
-            db.run('INSERT INTO Match (rounds, vencedor, time_um, time_dois) VALUES (?, ?, ?, ?)', [match.rounds, match.vencedor, match.time_um, match.time_dois]);
-        });
-    
-        res.json({
-            "statusCode": 200,
-            "message": "Success"
-        });
-    }
-    
-    updateMatch(req, res){
-        match.rounds = req.body.rounds;
-        match.vencedor = req.body.vencedor;
-        match.time_um = req.body.time_um;
-        match.time_dois = req.body.time_dois;
-        match.id = req.body.id;
-        openDb().then(db => {
-            db.run('UPDATE Match SET rounds=?, vencedor=?, time_um=?, time_dois=? WHERE id=?', [match.rounds, match.vencedor, match.time_um, match.time_dois, match.id]);
-        });
-    
-        res.json({
-            "statusCode": 200,
-            "message": "Success"
-        });
-    }
-    
-    selectMatches(req, res){
-       return openDb().then(db => {
-            return db.all('SELECT Match.id, Match.rounds,TimeVencedor.nome AS vencedor,TimeUm.nome AS time_um, TimeDois.nome AS time_dois FROM Match JOIN Time AS TimeVencedor ON Match.vencedor = TimeVencedor.id JOIN Time AS TimeUm ON match.time_um = TimeUm.id JOIN Time AS TimeDois ON Match.time_dois = TimeDois.id')    
-                .then(matchs => res.json(matchs));
-            });
-    }
-    
-    selectMatch(req, res){
-        match.id = req.body.id;
-        return openDb().then(db => {
-             return db.get('SELECT Match.id, Match.rounds,TimeVencedor.nome AS vencedor,TimeUm.nome AS time_um, TimeDois.nome AS time_dois FROM Match JOIN Time AS TimeVencedor ON Match.vencedor = TimeVencedor.id JOIN Time AS TimeUm ON Match.time_um = TimeUm.id JOIN Time AS TimeDois ON match.time_dois = TimeDois.id WHERE match.id=?', [match.id])
-             .then(match => res.json(match));   
-         });
-     }
-    
-    deleteMatch(req, res){
-        match.id = req.body.id;
-        openDb().then(db => {
-            db.get('DELETE FROM Match WHERE id=?', [match.id])
-            .then(res => res);
-         });
-    
-         res.json({
-            "statusCode": 200,
-            "message": "Success"
-        });
-     }
+class PartidaController{
+    createMatch(){
+      openDb().then(db => {
+          db.exec('CREATE TABLE IF NOT EXISTS Partida (id INTEGER PRIMARY KEY, rounds INTEGER, vencedor INTEGER, time_um INTEGER, time_dois,FOREIGN KEY(vencedor, time_um, time_dois) REFERENCES Time(id, id, id))');
+      });
+  }
+  
+  insertMatch(req, res){
+      partida.rounds = req.body.rounds;
+      partida.vencedor = req.body.vencedor;
+      partida.time_um = req.body.time_um;
+      partida.time_dois = req.body.time_dois;
+      openDb().then(db => {
+          db.run('INSERT INTO Partida (rounds, vencedor, time_um, time_dois) VALUES (?, ?, ?, ?)', [partida.rounds, partida.vencedor, partida.time_um, partida.time_dois]);
+      });
+  
+      res.json({
+          "statusCode": 200,
+          "message": "Success"
+      });
+  }
+  
+  updateMatch(req, res){
+      partida.rounds = req.body.rounds;
+      partida.vencedor = req.body.vencedor;
+      partida.time_um = req.body.time_um;
+      partida.time_dois = req.body.time_dois;
+      partida.id = req.body.id;
+      openDb().then(db => {
+          db.run('UPDATE Partida SET rounds=?, vencedor=?, time_um=?, time_dois=? WHERE id=?', [partida.rounds, partida.vencedor, partida.time_um, partida.time_dois, partida.id]);
+      });
+  
+      res.json({
+          "statusCode": 200,
+          "message": "Success"
+      });
+  }
+  
+  selectMatches(req, res){
+     return openDb().then(db => {
+          return db.all('SELECT Partida.id, Partida.rounds,TimeVencedor.nome AS vencedor,TimeUm.nome AS time_um, TimeDois.nome AS time_dois FROM Partida JOIN Time AS TimeVencedor ON Partida.vencedor = TimeVencedor.id JOIN Time AS TimeUm ON Partida.time_um = TimeUm.id JOIN Time AS TimeDois ON Partida.time_dois = TimeDois.id')    
+              .then(partidas => res.json(partidas));
+          });
+  }
+  
+  selectMatch(req, res){
+      partida.id = req.body.id;
+      return openDb().then(db => {
+           return db.get('SELECT Partida.id, Partida.rounds,TimeVencedor.nome AS vencedor,TimeUm.nome AS time_um, TimeDois.nome AS time_dois FROM Partida JOIN Time AS TimeVencedor ON Partida.vencedor = TimeVencedor.id JOIN Time AS TimeUm ON Partida.time_um = TimeUm.id JOIN Time AS TimeDois ON Partida.time_dois = TimeDois.id WHERE Partida.id=?', [partida.id])
+           .then(partida => res.json(partida));   
+       });
+   }
+  
+  deleteMatch(req, res){
+      partida.id = req.body.id;
+      openDb().then(db => {
+          db.get('DELETE FROM Partida WHERE id=?', [partida.id])
+          .then(res => res);
+       });
+  
+       res.json({
+          "statusCode": 200,
+          "message": "Success"
+      });
+   }
 }
 
-export default new matchController();
+export default new PartidaController();
