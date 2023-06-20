e7193f16adc431ae35b22a933c3deb7e280a78c6
Nzzinn <nf2086@gmail.com>
2023-06-20 09:06:39 -0300

    Https

--- server/src/app.js
+++ server/src/app.js
@@ -12,11 +12,7 @@ app.use(router);
 
 app.listen(7070, () => console.log(`Server running on port 7070`));    
 
-<<<<<<< HEAD
 https.createServer({
     cert: fs.readFileSync('server/src/SSL/code.crt'),
     key: fs.readFileSync('server/src/SSL/code.key')
 }, app).listen(8000, () => console.log("Server Running https on port 8000"));   
-=======
-app.listen(8000, () => console.log(`Server running on port 7070`));    
->>>>>>> 78d3d95a96ea3dc5c5992a98ddbce3c485be9957
