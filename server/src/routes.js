4dd812b5cddd49290caa58b8ee963aad0b38efd4
Gabriel Vieira Stutz <gabrielstutz100@gmail.com>
2023-06-19 22:09:28 -0300

    matches updated

--- server/src/routes.js
+++ server/src/routes.js
@@ -9,4 +9,10 @@ router.post('/teams/create', timeController.insertTeam);
 router.put('/teams/update', timeController.updateTeam);
 router.delete('/teams/delete', timeController.deleteTeam);
 
+router.get('/matches/all', partidaController.selectMatches);
+router.get('/matches/match', partidaController.selectMatch);
+router.post('/matches/create', partidaController.insertMatch);
+router.put('/matches/update', partidaController.updateMatch);
+router.delete('/matches/delete', partidaController.deleteMatch);
+
 export default router;
